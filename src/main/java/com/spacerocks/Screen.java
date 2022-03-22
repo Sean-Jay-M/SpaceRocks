e0ddd9602112f027b1dfc9fb6273605a536dccfa
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-03-22 21:02:10 +0000

    Adding comment to width / height

--- src/main/java/com/spacerocks/Screen.java
+++ src/main/java/com/spacerocks/Screen.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 // This class is responsible for handling the GUI elements of the game
 public class Screen {
 
+    // We may decide that these can be modifiable - to be discussed
     private static final int SCREEN_WIDTH = 500;
     private static final int SCREEN_HEIGHT = 500;
 
