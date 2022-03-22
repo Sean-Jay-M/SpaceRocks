b95443130ce6c7f49b0fe08547697ca462c951e6
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-03-22 20:47:18 +0000

    Refactoring, adding "Main" as a separate class.

---
+++ src/main/java/com/spacerocks/Main.java
@@ -0,0 +1,19 @@
+package com.spacerocks;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    // Main method for debugging. To be deleted once the Java project has been built
+    public static void main(String[] args) {
+        launch(args);
+        System.exit(0);
+    }
+
+    // Start method is the method that will launch the JavaFX application
+    public void start(Stage gameStage) {
+        Screen screen = new Screen(gameStage);
+        screen.createMainWindow();
+    }
+}
--- src/main/java/com/spacerocks/Screen.java
+++ src/main/java/com/spacerocks/Screen.java
@@ -1,42 +1,46 @@
 package com.spacerocks;
 
-import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 // This class is responsible for handling the GUI elements of the game
-public class Screen extends Application {
+public class Screen {
 
-    public static final int SCREEN_WIDTH = 500;
-    public static final int SCREEN_HEIGHT = 500;
+    private static final int SCREEN_WIDTH = 500;
+    private static final int SCREEN_HEIGHT = 500;
 
-    // Main method for debugging. To be deleted once the Java project has been built
-    public static void main(String[] args) {
-        launch(args);
-        System.exit(0);
+    // I added "final" to these because IntelliJ suggested it. If we ended up
+    // using more JavaFX elements we can remove this.
+    private final Stage gameStage;
+    private final Pane gamePane;
+    private final Scene gameScene;
+
+    public Screen(Stage gameStage) {
+        // Constructor creates new instances of these objects. The reason why we need
+        // the Stage as an argument is because the Main class automatically creates a
+        // Stage as part of the JavaFX "Start" method, so to avoid doubling up we can
+        // just use the Stage that that method created.
+        this.gameStage = gameStage;
+        gamePane = new Pane();
+        gameScene = new Scene(gamePane);
     }
 
-    // Start method is the method that will launch the JavaFX application
-    public void start(Stage mainStage) {
-
+    public void createMainWindow() {
         // Set title of the game window
-        mainStage.setTitle("SpaceRocks");
+        gameStage.setTitle("SpaceRocks");
 
-        // Creating pane and setting the preferred size
-        Pane mainWindow = new Pane();
-        mainWindow.setPrefSize(SCREEN_HEIGHT, SCREEN_WIDTH);
+        // Setting the size of the game pane
+        gamePane.setPrefSize(SCREEN_HEIGHT, SCREEN_WIDTH);
 
-        // Creating scene and setting background color to black
-        Scene mainScene = new Scene(mainWindow);
-        mainScene.setFill(Color.BLACK);
+        // Setting background color to black
+        gameScene.setFill(Color.BLACK);
 
         // Activating the scene on the stage when the application launches
-        mainStage.setScene(mainScene);
+        gameStage.setScene(gameScene);
 
         // Displaying the application
-        mainStage.show();
+        gameStage.show();
     }
-
 }
