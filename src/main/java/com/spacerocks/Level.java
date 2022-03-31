8dbf78e86d0d84040ac862872ed92b842d0fa456
Oscardbf <92519436+Oscardbf@users.noreply.github.com>
2022-03-31 15:43:20 +0100

    created Level class and some associated methods

---
+++ src/main/java/com/spacerocks/Level.java
@@ -0,0 +1,25 @@
+package com.spacerocks;
+
+//a Level object would be created when the game is started
+//reset the object on losing back to 1
+//increase when level has been won
+//get level at start of next level to spawn number of asteroids
+public class Level {
+    private int level;
+
+    public Level(){ //can only create object with level = 1
+        level = 1;
+    }
+
+    public void increaseLevel(){ //level can only be increased by one at a time (or reset)
+        level++;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+
+    public void resetLevel(){
+        level = 1;
+    }
+}
