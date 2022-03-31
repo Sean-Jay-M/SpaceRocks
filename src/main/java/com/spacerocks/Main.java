d4d707c7f7dc7b9bfe58c093a302ba4f4141ca32
ChihLun <59338015+Chih-lun@users.noreply.github.com>
2022-03-31 15:50:28 +0100

    Seperate game logic to Game object

--- src/main/java/com/spacerocks/Main.java
+++ src/main/java/com/spacerocks/Main.java
@@ -1,6 +1,5 @@
 package com.spacerocks;
 
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -14,45 +13,7 @@ public class Main extends Application {
 
     // Start method is the method that will launch the JavaFX application
     public void start(Stage gameStage) {
-
-        // Create the screen object and generate the main window
-        Screen screen = new Screen(gameStage);
-        Ship ship = new Ship(screen.getScreenWidth()/2,screen.getScreenHeight()/2);
-
-        // Creating the ship controller, passing in the Ship that we have created and the Scene property of the Screen.
-        Controller shipController = new Controller(ship, screen);
-
-        screen.createMainWindow();
-        screen.drawGameObject(ship);
-
-        // Starting new animation timer and setting up the controls inside to read user input continuously.
-        new AnimationTimer() {
-            @Override
-            public void handle(long l) {
-                shipController.initControls();
-                shipController.control();
-                // default speed of ship is 0, so the ship is moving all the time.
-                ship.move();
-
-                // if the ship is thrusting, the ship will accelerate. otherwise, it will slow down.
-                if (ship.isThrusting()){
-                    ship.accelerate();
-                } else{
-                    ship.slowDown();
-                }
-
-                // move all the bullets
-                for (Bullet bullet: ship.getBullets()){
-                    bullet.move();
-                    // if the bullet is over certain distance, it should be removed.
-                    if (bullet.getDistance() > 10){
-                        ship.getBullets().remove(bullet);
-                        screen.removeGameObject(bullet);
-                        // it is mandatory to break the loop. otherwise, it will crash.
-                        break;
-                    }
-                }
-            }
-        }.start();
+        Game game = new Game(gameStage);
+        game.play();
     }
 }
