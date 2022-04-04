344a1e7364e902c46ca6a8c9e58337a203f6328a
Oscardbf <92519436+Oscardbf@users.noreply.github.com>
2022-04-04 17:45:26 +0100

    added random spawn coordinate method

--- src/main/java/com/spacerocks/AlienShip.java
+++ src/main/java/com/spacerocks/AlienShip.java
@@ -2,10 +2,24 @@ package com.spacerocks;
 
 import javafx.scene.shape.Polygon;
 
+import static com.spacerocks.Screen.getScreenHeight;
+import static com.spacerocks.Screen.getScreenWidth;
+
 public class AlienShip extends GameObject {
 
-
     public AlienShip(double speed, double xposition, double yposition) {
-        super(new Polygon(-15, -15, 15, -15, 15, 15, 15, -15), speed, xposition, yposition);
+        super(new Polygon(-15, -15,
+                15, -15,
+                15, 15,
+                -15, 15), 2, xposition, yposition);
+
+        spawn_x = spawnCoord(getScreenWidth());
+        spawn_x = spawnCoord(getScreenHeight());
     }
+
+    private int spawnCoord(int upperBound){
+        return (int)(Math.random() * upperBound);
+    }
+
+
 }
