1473f05d20dc3bbd963e63cb1079520e12c828b1
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-04-07 07:32:15 +0100

    Added highscorehandler class, can currently read txt file.

---
+++ src/main/java/com/spacerocks/HighScoreHandler.java
@@ -0,0 +1,14 @@
+package com.spacerocks;
+
+import java.io.*;
+
+public class HighScoreHandler {
+    public void readScore() throws IOException {
+        FileReader reader = new FileReader("score.txt");
+        BufferedReader br = new BufferedReader(reader);
+        String currentLine;
+        while ((currentLine = br.readLine()) != null) {
+            System.out.println(currentLine);
+        }
+    }
+}
