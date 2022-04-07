c6f51f7ac13373ec65db0c9eb72c9d0e1775cb66
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-04-07 07:33:40 +0100

    Added neater directory for high score handler

--- src/main/java/com/spacerocks/HighScoreHandler.java
+++ src/main/java/com/spacerocks/HighScoreHandler.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class HighScoreHandler {
     public void readScore() throws IOException {
-        FileReader reader = new FileReader("score.txt");
+        FileReader reader = new FileReader("resources/score.txt");
         BufferedReader br = new BufferedReader(reader);
         String currentLine;
         while ((currentLine = br.readLine()) != null) {
