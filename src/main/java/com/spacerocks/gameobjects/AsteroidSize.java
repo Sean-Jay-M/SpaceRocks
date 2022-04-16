f342170b4c72344595aa6f7572152a07aeca088a
Sean-Jay-M <91853278+Sean-Jay-M@users.noreply.github.com>
2022-04-16 17:04:56 +0100

    Add the ENUM values

--- src/main/java/com/spacerocks/gameobjects/AsteroidSize.java
+++ src/main/java/com/spacerocks/gameobjects/AsteroidSize.java
@@ -11,7 +11,7 @@ public enum AsteroidSize {
             69/1.5, 175/1.5,
             25/1.5, 131/1.5,
             25/1.5, 69/1.5,
-            0.2),
+            2, 3),
     MEDIUM(69/2.5, 25/2.5,
             131/2.5, 25/2.5,
             175/2.5, 69/2.5,
@@ -20,7 +20,7 @@ public enum AsteroidSize {
             69/2.5, 175/2.5,
             25/2.5, 131/2.5,
             25/2.5, 69/2.5,
-            0.2),
+            1, 2),
     SMALL(  69/3.5, 25/3.5,
             131/3.5, 25/3.5,
             175/3.5, 69/3.5,
@@ -29,7 +29,7 @@ public enum AsteroidSize {
             69/3.5, 175/3.5,
             25/3.5, 131/3.5,
             25/3.5, 69/3.5,
-            0.2);
+            0.1, 1);
 
     final double x1;
     final double y1;
@@ -55,10 +55,11 @@ public enum AsteroidSize {
     //Get the centers
     void AsteroidCenter(double x4, double x8, double y5, double y2){
          double xcenter = (x4 - x8) / 2;
-         double ycenter = (y5 - y2) - 2;
+         double ycenter = (y5 - y2) / 2;
     }
+    
 
-    AsteroidSize(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, double x5, double y5, double x6, double y6, double x7, double y7, double x8, double y8, double speed) {
+    AsteroidSize(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, double x5, double y5, double x6, double y6, double x7, double y7, double x8, double y8, double min, double max) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
@@ -75,6 +76,7 @@ public enum AsteroidSize {
         this.y7=y7;
         this.x8 = x8;
         this.y8 = y8;
-        this.speed = speed;
+        this.min = min;
+        this.max = max;
     }
 }
