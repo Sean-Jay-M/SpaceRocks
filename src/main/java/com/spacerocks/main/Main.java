e8fcf5e670ff1755ea6b874a2b5d28afe788602c
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-04-17 18:16:25 +0100

    Added Main comments

--- src/main/java/com/spacerocks/main/Main.java
+++ src/main/java/com/spacerocks/main/Main.java
@@ -1,21 +1,23 @@
+// Class is part of main package
 package com.spacerocks.main;
 
+// Importing necessary JavaFX elements
 import javafx.application.Application;
 import javafx.stage.Stage;
-import java.io.IOException;
 
+// This class is responsible for starting the application.
 public class Main extends Application {
 
-    // Main method for debugging. To be deleted once the Java project has been built
+    // Main method for debugging. This will be deleted if this game were ever to go into production.
     public static void main(String[] args) {
         launch(args);
         System.exit(0);
     }
 
-    // Start method is the method that will launch the JavaFX application
-    public void start(Stage gameStage) throws IOException {
-//        Media media = new Media("file:music/ost.mp3");
-//        MediaPlayer mediaPlayer
+    // Start method is the method that will launch the JavaFX application.
+    // When starting the application, we should create the screen, initialise the default
+    // screen, and start with the main menu.
+    public void start(Stage gameStage) {
         Screen screen = Screen.getScreenInstance();
         screen.initScreen(gameStage);
         screen.setMenuScreen();
